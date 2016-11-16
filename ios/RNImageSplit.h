02c20b1ab6fc150721b73c8be1008ce7ae4c5f6f
Wang Zi Xiao <328249923@qq.com>
2016-11-16 14:42:59 +0800

    图片拼接

---
+++ ios/RNImageSplit.h
@@ -0,0 +1,15 @@
+//
+//  RNImageSplit.h
+//  RNImageSplit
+//
+//  Created by wangzixiao on 16/11/14.
+//  Copyright © 2016年 WangZiXiao. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface RNImageSplit : NSObject <RCTBridgeModule>
+
+@end
