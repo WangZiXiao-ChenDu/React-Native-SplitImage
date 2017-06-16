4ca52c0abb31dcf97180ea72c83b860423bfbade
Wang Zi Xiao <328249923@qq.com>
2017-06-16 17:34:46 +0800

    support RN-0.40

--- ios/RNImageSplit/RNImageSplit.h
+++ ios/RNImageSplit/RNImageSplit.h
@@ -6,7 +6,12 @@
 //  Copyright © 2016年 WangZiXiao. All rights reserved.
 //
 
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
 #import "RCTBridgeModule.h"
+#endif
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
